85ac38d19823c98bd33af536357cee18491f14e0
vivianna ramirez <vivrami2001@gmail.com>
2023-04-08 15:45:51 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = ( parameter1, parameter2) => parameter1/ parameter2
+    divide(2000,100);
+const square= (parameter1) => parameter1*parameter1
+    multiply()
+function add(parameter1, parameter2) {
+    return (parameter1 + parameter2);
+}
